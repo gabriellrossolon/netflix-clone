18fc4cc1aa6ac646d1852c4e23b95986ce2929e9
Gabriell Rossolon <gabrirossolon@yahoo.com.br>
2024-03-04 14:49:11 -0400

    first commit

---
+++ src/Tmdb.js
@@ -0,0 +1,67 @@
+const API_KEY = 'bfb62d959c697d144af2215498144da4';
+const API_BASE = 'https://api.themoviedb.org/3';
+
+
+/*
+- originais da netflix
+- recomendados (trending)
+- em alta (top rated)
+- ação
+- comédia
+- fantasia
+- terror
+- romance
+*/
+
+const basicFetch = async (endpoint) => {
+    const req = await fetch(`${API_BASE}${endpoint}`);
+    const json = await req.json();
+    return json;
+}
+
+export default {
+    getHomeList: async () => {
+        return [
+            {
+                slug: 'originals',
+                title: 'originais do Netflix',
+                items: await basicFetch(`/discover/tv?with_network=213&language=pt-BR&api_key=${API_KEY}`)
+            },
+            {
+                slug: 'trending',
+                title: 'Recomendados para você',
+                items: await basicFetch (`/trending/all/week?language=pt-BR&api_key=${API_KEY}`)
+            },
+            {
+                slug: 'toprated',
+                title: 'Recomendados para você',
+                items: await basicFetch (`/movie/top_rated?language=pt-BR&api_key=${API_KEY}`)
+            },
+            {
+                slug: 'action',
+                title: 'Açãi',
+                items: await basicFetch (`/discover/movie?with_genres=28&language=pt-BR&api_key=${API_KEY}`)
+            },
+            {
+                slug: 'comedy',
+                title: 'Comédia',
+                items: await basicFetch (`/discover/movie?with_genres=35&language=pt-BR&api_key=${API_KEY}`)
+            },
+            {
+                slug: 'horror',
+                title: 'Terror',
+                items: await basicFetch (`/discover/movie?with_genres=27&language=pt-BR&api_key=${API_KEY}`)
+            },
+            {
+                slug: 'romance',
+                title: 'Romance',
+                items: await basicFetch (`/discover/movie?with_genres=10749&language=pt-BR&api_key=${API_KEY}`)
+            },
+            {
+                slug: 'documentary',
+                title: 'Documentários',
+                items: await basicFetch (`/discover/movie?with_genres=99&language=pt-BR&api_key=${API_KEY}`)
+            },
+        ]
+    }
+}
